039ee260ea153f0ef53dcdadb3f86897142ebb54
Krogh-Moe <espen.krogh-moe@finn.no>
2016-05-20 10:13:54 +0200

    Added support for namespaces

--- k8s/kexec.sh
+++ k8s/kexec.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 podname=$(kubectl get pods --all-namespaces | grep -v Terminating | awk -F ' ' '{print $2}' | grep $1 | head -1)
+namespace=$(kubectl get pods --all-namespaces | grep -v Terminating | grep $1 | awk -F ' ' '{print $1}'|  head -1)
 givencommand=$2
 cmd=${givencommand:-"bash"}
-kubectl -it exec $podname $cmd
+kubectl --namespace=$namespace -it exec $podname $cmd
--- k8s/klog.sh
+++ k8s/klog.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-kubectl get pods --all-namespaces | grep $1 | grep -v Terminating | head -1 | awk -F ' ' '{print $2}' | xargs kubectl logs -f
+namespace=$(kubectl get pods --all-namespaces | grep -v Terminating | grep $1 | awk -F ' ' '{print $1}'|  head -1)
+kubectl get pods --all-namespaces | grep $1 | grep -v Terminating | head -1 | awk -F ' ' '{print $2}' | xargs kubectl logs --namespace=$namespace -f
