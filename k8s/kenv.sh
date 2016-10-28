8071810adcfe3614115a8f7dbcf4ca9e4adbd720
Krogh-Moe <espen.krogh-moe@finn.no>
2016-10-28 09:25:11 +0200

    Added --show-all

--- k8s/kenv.sh
+++ k8s/kenv.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
+if [ "$1" == "list" ]; then
+    kubectl config view -o jsonpath='{.contexts[*].name}' | tr " " "\n"
+    echo
+    exit
+fi
+
 if [ "$#" -eq 1 ]; then
     kubectl config use-context $1
 fi
+
 
 if [ "$#" -eq 0 ]; then
     kubectl config view -o jsonpath='{.current-context}'
 fi
 
-echo
-kubectl  cluster-info
-
+#echo
+#kubectl  cluster-info
--- k8s/klog.sh
+++ k8s/klog.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-namespace=$(kubectl get pods --all-namespaces | grep -v Terminating | grep $1 | awk -F ' ' '{print $1}'|  head -1)
-kubectl get pods --all-namespaces | grep $1 | grep -v Terminating | head -1 | awk -F ' ' '{print $2}' | xargs kubectl logs --namespace=$namespace -f
+namespace=$(kubectl get pods --all-namespaces --show-all | grep -v Terminating | grep $1 | awk -F ' ' '{print $1}'|  head -1)
+kubectl get pods --all-namespaces --show-all | grep $1 | grep -v Terminating | head -1 | awk -F ' ' '{print $2}' | xargs kubectl logs --namespace=$namespace -f
